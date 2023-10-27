cfa6306a667e95dcf00b778010dde5553e65c2f4
Ivantg01 <96993149+Ivantg01@users.noreply.github.com>
2023-10-27 18:22:41 +0200

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,25 +1,28 @@
 //Practica 3A - Spin me like a record
 
+import express, { Request, Response } from "npm:express@4.18.2";
+
 //cargamos el fichero de entorno .env con la URI de la base de datos
 import { load } from "https://deno.land/std@0.202.0/dotenv/mod.ts";
 const env = await load();
-const DB_URI = env["DB_URI"] ||/* Deno.env.get("DB_URI") || */"mongodb://localhost:27017/mascotas" //3000
-//const DB_URI="mongodb+srv://userdb:pass4userdb@cluster0.m7bq9ap.mongodb.net/?retryWrites=true&w=majority"
+
+//const DB_URI = env["DB_URI"] || Deno.env.get("DB_URI") || "mongodb://localhost:27017/mascotas"
+const DB_URI=env.DB_URI
 
 //conexion con la base de datos
 //import mongoose from "npm:mongoose@^6.7";
 import mongoose from "npm:mongoose@7.6.3";
 try {
   console.log("Database: connecting... ", DB_URI);
-  const db = await mongoose.connect(DB_URI);
-  console.log("Database: connected", db.connection.name);
+  await mongoose.connect(DB_URI);
+  console.log("Database: connected");
 } catch (error) {
   console.log("Database: error: ", error);
 }
 
 
 //inicialiamos aplicacion web
-import express, { Request, Response } from "npm:express@4.18.2";
+
 const app = express();
 app.use(express.json());  //habilitamos el uso de json
 
@@ -32,19 +35,19 @@ app
       res.status(200).send("API para manejar mascotas!");
     })
     //-Obtener todos los discos existentes
-    .get("/mascotas/", getMascotas)
+    .get("/api/mascotas/", getMascotas)
 
     //-Obtener un disco mediante id
-    .get("/mascotas/:id/", getMascotaByID)
+    .get("/api/mascotas/:id/", getMascotaByID)
     //Llamadas POST ->
     //-Crear nueva mascota
-    .post("/mascotas", addMascota)
+    .post("/api/mascotas", addMascota)
     //Llamadas PUT ->
     //-Actualizar una mascota existente indicándolo por su id
-    .put("/mascotas/:id",updateMascota)
+    .put("/api/mascotas/:id",updateMascota)
     //Llamadas DELETE ->
     //-Eliminar una mascota mediante su id
-    .delete("/mascotas/:id", deleteMascota);
+    .delete("/api/mascotas/:id", deleteMascota);
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
--- models.ts
+++ models.ts
@@ -2,22 +2,23 @@ import mongoose from 'npm:mongoose@7.6.3';
 
 const Schema = mongoose.Schema;
 
-type Disc = {
+
+type Mascota = {
     name: string,
     description: string,
-    type: string
+    type: string,
 }
 
 const mascotaSchema = new Schema(
     {
         name: String,
         description: String,
-        type: String,
+        type: String
         //no incluimos id ya que mongoDB crea un campo _id automaticamente
     },
-    { timestamps: true }
+    { timestamps: true } //createdAt, updatedAt
 );
 
-export type MascotaModelType = mongoose.Document & Omit<Disc, "id">;
+export type MascotaModelType = mongoose.Document & Omit<Mascota, "id">;
 
 export default mongoose.model<MascotaModelType>("Mascota", mascotaSchema);
